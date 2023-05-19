843f19d67d5a2d4b23c15fb6e892669c1dda56cd
Behnam Hajian <b.hajian@gmail.com>
2023-05-19 01:42:06 -0400

    fixes env

--- src/backend/aws-exports-dev.js
+++ src/backend/aws-exports-dev.js
@@ -2,10 +2,10 @@ import {Auth} from "aws-amplify";
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_appsync_graphqlEndpoint": "https://eo7ixgzfavgknfd4fuzzoszuai.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://keiak4c5vvgxraldm24md3mklq.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-i42ex6js3ra6jblqypgmbefxim",
+    "aws_appsync_apiKey": "da2-d2t3lw4oevdv7ek53nkqyvskfy",
     Auth: {
 
         // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
